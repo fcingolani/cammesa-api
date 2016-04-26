64d41387a0e519244a88cf6d0d197c7e70df6d29
Federico Cingolani <mail@fcingolani.com.ar>
2016-04-26 00:32:01 -0300

    Se agrega Web API básica

---
+++ bin/server.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+"use strict";
+
+var restify = require('restify');
+
+let CammesaAPI = require('..');
+
+let api = new CammesaAPI;
+
+let server = restify.createServer();
+
+server.get('/', (req, res, next) => {
+  api.getSources()
+    .then( sources => res.send(sources) )
+    .catch(next);
+});
+
+server.get('/:source', (req, res, next) => {
+  api.getSeries(req.params.source)
+    .then( series => res.send(series) )
+    .catch(next);
+});
+
+server.get('/:source/:serie', (req, res, next) => {
+  api.getDataPoints(req.params.source, req.params.serie)
+    .call('sort', (a,b) => a.date.localeCompare(b.date) )
+    .then( dataPoints => res.send(dataPoints) )
+    .catch(next);
+});
+
+server.listen(process.env.port || 80, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
